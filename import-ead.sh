8e27a875ca884fce679ec4c691ba4f88be7e51c7
Gioele Barabucci <gioele.barabucci@uni-koeln.de>
2014-09-22 17:13:25 +0200

    Add script to import EAD dumps into AtoM

---
+++ import-ead.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+set -e
+set -u
+
+version="stable"
+#version="git"
+version="mea"
+
+A="atom-2.0.1"
+[ "$version" = "git" ] && A="atom-qa-2.1.x"
+[ "$version" = "mea" ] && A="atom-qa-2.1.x-mea"
+
+printf "%s" "About to import data into Atom ($version) = '$A' [press ENTER]" ; read enter
+
+cd "$A"
+
+sentinel_file='config/search.yml'
+if [ ! -f "$sentinel_file" ] ; then
+	echo "ERROR: Atom ($version) in '$A' is not configured: '$sentinel_file' not found."
+	exit 1
+fi
+
+ead_dir="../ead-imports/import6"
+
+echo "Importing EAD from directory '$ead_dir'..."
+./symfony import:bulk "$ead_dir"
+
+echo "Populating the search indexes..."
+./symfony search:populate
+
+echo "Crearing the cache..."
+./symfony cache:clear
